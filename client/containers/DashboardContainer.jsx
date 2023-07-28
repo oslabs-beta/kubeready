7513ed464762426ab9dda637306dbec235f274b9
Diane Moon <dianejmoon@gmail.com>
2023-07-28 10:07:22 -0400

    dashboard container

--- client/containers/DashboardContainer.jsx
+++ client/containers/DashboardContainer.jsx
@@ -7,7 +7,7 @@ const DashboardContainer = () => {
       <p>dashboard container</p>
       <iframe 
         title='' 
-        src="" 
+        src="http://localhost:3000/d/d84d95bb-d3cc-4353-baba-0893b6dc0ba0/my-dashboard?orgId=1&refresh=5s" 
         height="700px" 
         width="700px"
         >
