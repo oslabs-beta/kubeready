80fe400f5431c90119a27da7df8d95b4844719c6
noel <njpallivathucal@gmail.com>
2023-07-31 15:40:52 -0700

    added a grafanacontroller

--- client/containers/DashboardContainer.jsx
+++ client/containers/DashboardContainer.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 const DashboardContainer = () => {
-
   return (
     <div>
       <p>dashboard container</p>
-      <iframe 
-        title='' 
-        src="" 
-        height="700px" 
-        width="700px"
-        >
-      </iframe>
+      <iframe
+        title=''
+        src='http://127.0.0.1:57640/d/b70170ce-5631-4b0f-b8f2-d1c92bbff59b/kubernetes-cluster-visual?orgId=1&from=1690412518649&to=1690412818649'
+        height='700px'
+        width='700px'
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
 export default DashboardContainer;
