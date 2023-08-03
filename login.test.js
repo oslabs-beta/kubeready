38186d7cd8beeea8720e13d403ea06c82306c82c
alanaherlands <alana@herlands.com>
2023-08-03 19:21:06 -0400

    write more login tests

--- login.test.js
+++ login.test.js
@@ -8,9 +8,10 @@ import userEvent from '@testing-library/user-event';
 // import the login function (or whatever it's actually called)
 const { } = require('');
 
-beforeEach(() => {
+beforeAll(() => {
   // not sure what to put here
   // listen for server?
+  server.listen();
 })
 
 afterEach(() => {
@@ -46,14 +47,24 @@ describe('login page', () => {
     })
     test('user is navigated to dashboard after entering correct login credentials', async () => {
       // create a login button, username, and pass
-
+      const loginButton = screen.getByRole('button', {name: 'Login'});
+      const userInput = screen.getByPlaceholderText('Username')
+      const passwordInput = screen.getByPlaceholderText('Password');
       // user types in correct user and pass
-
+      await userEvent.type(userInput, 'testuser')
+      await userEvent.type(passwordInput, 'testpassword')
       // assert the correct user and pass
-
+      expect(userInput.value).toBe('testuser')
+      expect(passwordInput.value).toBe('testpassword')
       // user clicks login button
-
+      await userEvent.click(loginButton);
       // assert that the dashboard is in the document
+      waitFor(async() => {
+        // *NOTE* REPLACE IFRAME NAME HERE
+        await screen.findByTitle('NAME OF IFRAME')
+        // assertion: expect the iframe to be in the document
+        expect(screen.findByTitle('NAME OF THE IFRAME')).toBeInTheDocument();
+      })
     })
     test ('user sees error message after loggin in with incorrect credentials', () => {
       // creat a login button, username, and pass
--- signup.test.js
+++ signup.test.js
@@ -4,6 +4,10 @@ import React from 'react';
 //import SignUpPage from
 //use react testing library: provides virtual DOM environment to render and interact with components during testing
 import { render, screen, fireEvent } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
+//docs: jest, react testing library, react router DOM, stack overflow. kubernet 
+
+//react router dom: will handle routing and navigation between different pages or views 
 
 //describe: used to create test suite in our case Sign Up page unit tests live within describe block  
 
@@ -11,10 +15,13 @@ describe('signUpPage', () => {
   //does sign up page render form elements saved userInput, passwordInput, submitButton?
   test('renders sign-up form elements', () => {
     //renders signup page component in virtual DOM 
-      render(<SignUpPage />);
-      //querying virtual DOM to select element associated with what is inside getLabelByText
-        //use screen queries to find elements
-    const nameInput = screen.getByLabelText('Name');
+      render(
+        <MemoryRouter>
+          <SignUpPage />
+        </MemoryRouter>
+      );
+    //querying virtual DOM to select element associated with what is inside getLabelByText, using screen queries to find elements
+      const nameInput = screen.getByLabelText('Name');
       const userInput = screen.getByLabelText('Username');
       const passwordInput = screen.getByLabelText('Password');
       //query virtual DOM to find role button and  look for input associated with it 
@@ -25,43 +32,102 @@ describe('signUpPage', () => {
       expect(passwordInput).toBeInTheDocument()
       expect(submitButton).toBeInTheDocument()
     })
+    
 //test: is an error message appearing if un or pw is missing?
 test('display error if all input fields not filled', () => {
     //renders signup page component in jest environment 
-    render(<SignUpPage />);
+    render(
+      <MemoryRouter>
+        <SignUpPage />
+      </MemoryRouter >
+    );
+
     const submitButton = screen.getByRole('button', {name: 'Sign Up'});
     //simulate submit form funcitonality
     fireEvent.click(submitButton);
+    
     //query virtual DOM to find element that has id set to error 
     const errorMessage = screen.getByTestId('error');
     //write expect cases
-    expect(errorMessage).toBeInDocument();
+    expect(errorMessage).toBeInTheDocument();
     expect(errorMessage.textContent).toBe("All input fields are required to be filled on form submission.")
 
 })
   //test: is the error message removed upon a correct un and pw entry and submission?
   test('error message is removed when form submit is sucessful');
-  render(<SignUpPage />);
+  render(
+   <MemoryRouter>
+     <SignUpPage />
+   </MemoryRouter>
+  );
    //querying virtual DOM to select element associated with what is inside getLabelByText
     const userInput = screen.getByLabelText('Username');
       const passwordInput = screen.getByLabelText('Password');
-      const nameInput = screen.getByLabelText('test');
+      const nameInput = screen.getByLabelText('Name');
       //query virtual DOM to find role button and  look for input associated with it 
       const submitButton = screen.getByRole('button', {name: 'Sign Up'});
-      //simulate when userInput field has changed 
-      //fireEvent mimics a user typing into a field
-      //set property of input fields to what user has typed into what is in value field
-      fireEvent.change(nameInput, {target: {value: 'Noel'}})
-      fireEvent.change(userInput, {target: {value: 'user1'}})
-      fireEvent.change(passwordInput, {target: {value: 'pass1'}})
+      // simulate user input in each of the input fields setting their values to 'testname', 'testuser', and 'testpass'.
+      fireEvent.change(nameInput, {target: {value: 'testname'}})
+      fireEvent.change(userInput, {target: {value: 'testuser'}})
+      fireEvent.change(passwordInput, {target: {value: 'testpass'}})
+      //simulate form submit 
       fireEvent.click(submitButton)
-    //look for element with attribute set to error message 
+      //look for element with id set to error message 
       const errorMessage = screen.queryByTestId('error-message')
       //expecting form submission to be successful so should not see error message in virutal DOM so result should be null
       expect(errorMessage).toBeNull();
+})
+
+
+  //final test singup try and use jests async await fxnility instead of set timeout
+  //check behavior of SignUp component when successful form submit happens
+  test('display a success message and redirects to login page after form submits successfully', async () => {
+      
+    // mock form submission function
+    const mockFormSubmit = jest.fn(() => Promise.resolve());
+    
+    // render the SignUpPage component within the MemoryRouter
+    // create virtual environment for routing and navigation
+    // render signup page within this virtual environment
+    // pass the mockFormSubmit function as a prop to the SignUpPage component 
+    render(
+      <MemoryRouter>
+        <SignUpPage handleSubmit={mockFormSubmit}/>
+      </MemoryRouter>
+      );
+
+    // get elements from the rendered SignUpPage component
+    const nameInput = screen.getByLabelText('Name');
+    const userInput = screen.getByLabelText('Username');
+    const passwordInput = screen.getByLabelText('Password');
+    const submitButton = screen.getByRole('button', {name: 'Sign Up'});
+      
+    // simulate change events on form inputs to update their values
+    fireEvent.change (nameInput, {target: {name: 'name', value: 'testname' }})
+    fireEvent.change (userInput, {target: {name: 'username', value: 'testuser' }})
+    fireEvent.change (passwordInput, {target: {name: 'password', value: 'testpassword' }})
+
+    //simulate click event triggers form submission
+    fireEvent.click(submitButton);
+
+    // check if the success message is displayed after form submission
+    const successMessage = screen.getByTestId('success message');
+    expect(successMessage).toBeInTheDocument();
+
+    // check if the login page is rendered after successful form submission
+    const loginPage = screen.getByText('Login Page');
+    expect(loginPage).toBeInTheDocument();
+
+    //write expectations for mock fxn how many times it is called and what args its called w/
+    expect(mockFormSubmit).toBeCalledTimes(1);
+    expect(mockFormSubmit).toBeCalledWith({
+      name: 'testname',
+      username: 'testuser',
+      password: 'testpassword',
+    })
   });
 
 
-  //final test 
-  
+
+
   //run tests by command: npm jest fileName
