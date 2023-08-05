cb04ece4511dd9504af5a209a7ff29549e8d60b2
alanaherlands <alana@herlands.com>
2023-08-05 11:16:39 -0400

    added more server tests and made separate authentication test file

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,35 +1,72 @@
 // require in dependencies
+// import the 'supertest' library for testing HTTP endpoints
 const request = require('supertest');
-const server = 'http://localhost:3000'
+// set server URL to localhost 5050
+const server = 'http://localhost:5050'
+// generate test username with the current timestamp
+const testUserName = `test${Date.now()}`;
 
+// before running any tests, perform a signup request to create a test user
+beforeAll(() => {
+  // send a POST request to the signup endpoint with the test username and password and return the result as a promise
+  return request(server)
+    .post('/signup')
+    .send({ username: testUserName, password: '12345'});
+});
 
-// before all
+// after all tests are completed, delete the test user created during signup
+afterAll(() => {
+  // send a POST request to the delete endpoint with the test username and password and return the result as a promise
+  return request(server)
+    .post('/delete')
+    .send({ username: testUserName, password: '12345'});
+});
 
-
-// after all
-
-
-
-
-
-it('should return a valid authentication token when given correct credentials', async () => {
-    // create mock valid credentials
-    const validCredentials = {
-        username: 'valid_user',
-        password: 'valid_pass'
-    };
-    // assign result to invocation of login passing in the user and pass
-    const result = await login(validCredentials.username, validCredentials.password);
-    // assertion: expect the result's token to equal 'mock_token'
-    expect(result.token).toEqual('mock_token');
+// backend tests
+describe('backend', () => {
+  // signup route tests
+  describe('signup route', () => {
+    it('should return 400 if username already exists', () => {
+      // send a POST request to the signup endpoint with the same test username and expect the response status code to be 400 and the content-type header to be application/json
+      return request(server)
+        .post('/signup')
+        .send({ username: testUserName, password: '12345'})
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+    });
+    
+    it('should return specific error message for invalid signup', () => {
+      // send a POST request to the signup endpoint with an invalid payload (missing password)
+      return request(server)
+        .post('/signup')
+        .send({ username: 'test' })
+        .expect(422)
+        .expect('Content-Type', /application\/json/)
+        .expect((res) => {
+          // expect the response body to contain an 'error' property with a specific error message
+          expect(res.body.error).toEqual('Password is required.');
+        });
+    });
   });
-  
-  it('should throw an error when logging in with incorrect credentials', async () => {
-  // create mock invalid credentials
-  const invalidCredentials = {
-    username: 'invalid_user',
-    password: 'invalid_pass'
-  };
-  // expect the login function passing in the invalid user and pass to reject and throw an error
-  await expect(login(invalidCredentials.username, invalidCredentials.password)).rejects.toThrow('Invalid username and password');
+
+  // login route tests
+  describe('login route', () => {
+    it('should successfullyl login to a test account', () => {
+      // send a POST request to the login endpoint with the test username and password and expect the response status code to be 201 and the content-type header to be application/json
+      return request(server)
+        .post('/login')
+        .send({ username: testUserName, password: '12345'})
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    }, 25000) // set a timeout of 25000 ms (25 s)
+
+    it('should return 401 with invalid user credentials', () => {
+      // send a POST request to the login endpoint with the invalid user credentials and expect the response status code to be 401 and the content-type header to be application/json
+      return request(server)
+        .post('/login')
+        .send({ username: 'test' })
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
+    });
   });
+});
